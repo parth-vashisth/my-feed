7cf01fcbabb3127a523a84a41b848751f665ed93
parth vashisth <shrmaparth123@gmail.com>
2024-03-28 16:51:12 +0530

    responsive done

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,10 @@ switch1.addEventListener("click", function () {
     dot1.classList.toggle("dot-right");
     switch1.classList.toggle("switch-right")
 })
+
+
+let menuicon = document.querySelector(".menuicon");
+let lg_view = document.querySelector(".lg_view");
+menuicon.addEventListener("click", () => { 
+    lg_view.classList.toggle("show");
+});
